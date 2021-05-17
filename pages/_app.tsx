09ffda269e3c3f2b4d93204b6e980ac4d1e934fc
@Wil <emma.wilinston@gmail.com>
2021-05-17 17:45:49 -0500

    se agrega antd design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import 'antd/dist/antd.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />
