01a5b36b5cb240cb54adf719d236a05e1718a37b
@Wil <emma.wilinston@gmail.com>
2021-05-19 10:26:39 -0500

    se crea la pagina Footer

---
+++ Components/Footer.tsx
@@ -0,0 +1,34 @@
+import { Col, Row } from 'antd'
+
+const Footer = (): JSX.Element => {
+  return (
+    <Row justify='center' className='footer'>
+      <Col>
+        <Row>
+          <p>
+            Creado por <strong className='footer_name'>Wilinston Quispe</strong>{' '}
+            | ® 2021 Todos los derechos reservados.
+          </p>
+        </Row>
+        <Row justify='center'>
+          <p>
+            <a href='https://web.facebook.com/wil.quispe.1614' target='_blank'>
+              <img className='footer_social' src='/facebook.svg' />
+            </a>
+            <a href='https://twitter.com/WilQuispe3' target='_blank'>
+              <img className='footer_social' src='/twitter.svg' />
+            </a>
+            <a
+              href='https://www.youtube.com/channel/UC2xLpPmnDyAxvHlOTOdt_Ug'
+              target='_blank'
+            >
+              <img className='footer_social' src='/youtube.svg' />
+            </a>
+          </p>
+        </Row>
+      </Col>
+    </Row>
+  )
+}
+
+export default Footer
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import About from '../Components/About'
 import Contact from '../Components/Contact'
+import Footer from '../Components/Footer'
 import Inicio from '../Components/Inicio'
 import Navbar from '../Components/Navbar'
 import Proyects from '../Components/Proyects'
@@ -13,6 +14,7 @@ const IndexPage = () => (
     <Skills />
     <Proyects />
     <Contact />
+    <Footer />
   </>
 )
 
