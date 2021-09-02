51178df0bc7a3597b87e4dd04d9f92a6e96c6456
@Wil <emma.wilinston@gmail.com>
2021-09-02 18:55:14 -0500

    arreglo formulario

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -39,27 +39,27 @@ const Contact = () => {
         'service_jcuzbli',
         'template_m6xltea',
         e.target as HTMLFormElement,
-        'user_0Hjr5WWn82n2t2F8e1uxT'
+        'user_0Hjr5WWn82n2t2F8e1uxT',
       )
       .then(
         () => {
           message.success(`Correo enviado`)
           location.reload()
         },
-        error => {
+        (error) => {
           message.error('Fallo al enviar' + error)
-        }
+        },
       )
   }
 
   return (
-    <Row className='contact' justify='center' align='middle' id='contact'>
+    <Row className="contact" justify="center" align="middle" id="contact">
       <Col style={{ width: '80%' }}>
-        <NavHead txtMain='Contacto' txt='get in touch' />
-        <Row style={{ width: '100%' }} gutter={[16, 16]} justify='space-around'>
+        <NavHead txtMain="Contacto" txt="get in touch" />
+        <Row style={{ width: '100%' }} gutter={[16, 16]} justify="space-around">
           <Col {...gridMain}>
             <Row>
-              <h2 className='contact_h2'>Contactame</h2>
+              <h2 className="contact_h2">Contactame</h2>
               <p>
                 Para contactarte conmigo puedes enviar un correo llenando el
                 formulario o por las redes sociales. Importante solo para
@@ -69,48 +69,48 @@ const Contact = () => {
             <Row>
               <Col>
                 <ContactItem
-                  img='man.svg'
-                  txt='Wilinston Quispe'
-                  txtMain='Nombre'
+                  img="man.svg"
+                  txt="Wilinston Quispe"
+                  txtMain="Nombre"
                 />
                 <ContactItem
-                  img='gmail.svg'
-                  txt='newwilcode@gmail.com'
-                  txtMain='Correo'
+                  img="gmail.svg"
+                  txt="newwilcode@gmail.com"
+                  txtMain="Correo"
                 />
                 <ContactItem
-                  img='placeholder.svg'
-                  txt='Lima Peru'
-                  txtMain='Direccion'
+                  img="placeholder.svg"
+                  txt="Lima Peru"
+                  txtMain="Direccion"
                 />
               </Col>
             </Row>
           </Col>
           <Col {...gridMain}>
-            <h2 className='contact_h2'>Enviame un mensaje</h2>
+            <h2 className="contact_h2">Enviame un mensaje</h2>
             <form onSubmit={sendEmail}>
-              <Row justify='center'>
+              <Row justify="center">
                 <Col {...gridForm}>
                   <Form.Item
                     style={{ marginRight: '.5em' }}
-                    name='name'
+                    name="name"
                     rules={[{ required: true, message: 'campo requerido!' }]}
                   >
                     <Input
-                      name='name'
-                      placeholder='Nombre'
+                      name="name"
+                      placeholder="Nombre"
                       style={{ ...styleForm }}
                     />
                   </Form.Item>
                 </Col>
                 <Col {...gridForm}>
                   <Form.Item
-                    name='subject'
+                    name="subject"
                     rules={[{ required: true, message: 'campo requerido!' }]}
                   >
                     <Input
-                      name='subject'
-                      placeholder='Asunto'
+                      name="subject"
+                      placeholder="Asunto"
                       style={{ ...styleForm }}
                     />
                   </Form.Item>
@@ -118,7 +118,7 @@ const Contact = () => {
               </Row>
 
               <Form.Item
-                name='email'
+                name="email"
                 rules={[
                   {
                     required: true,
@@ -131,26 +131,26 @@ const Contact = () => {
                 ]}
               >
                 <Input
-                  name='email'
-                  placeholder='Correo'
+                  name="email"
+                  placeholder="Tu correo"
                   style={{ ...styleForm }}
                 />
               </Form.Item>
 
               <Form.Item
-                name='message'
+                name="message"
                 rules={[{ required: true, message: 'campo requerido!' }]}
               >
                 <Input.TextArea
-                  name='message'
-                  placeholder='Mensaje...'
+                  name="message"
+                  placeholder="Mensaje..."
                   style={{ ...styleForm }}
                 />
               </Form.Item>
 
-              <Row justify='center'>
-                <Button type='primary' htmlType='submit'>
-                  Submit
+              <Row justify="center">
+                <Button type="primary" htmlType="submit">
+                  Enviar
                 </Button>
               </Row>
             </form>
