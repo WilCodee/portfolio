d206b64427b9ac7a09da7ba367599400ecbf59dd
@Wil <emma.wilinston@gmail.com>
2022-04-27 15:48:54 -0500

    se hace el reserve en los proyects

--- Components/Proyects.tsx
+++ Components/Proyects.tsx
@@ -4,6 +4,108 @@ import { LanguageContext } from '../Context/LanguageContext'
 import NavHead from './atoms/NavHead'
 import ProyectsCard from './atoms/ProyectsCard'
 
+const DATA = [
+  {
+    img: ['/img/Inicio-Goshop.png'],
+    title: 'Ecommerce venta de Ropas',
+    titleEnglish: 'Ecommerce sale of clothes',
+    href: 'https://mi-tienda-online.vercel.app/',
+    dataList: [
+      'JavaScript',
+      'React.js',
+      'Redux',
+      'Antdesign',
+      'Css',
+      'Bulma',
+      'Next.js',
+      'Git',
+      'Apollo Client',
+      'Paypal',
+      'Cloudinary',
+    ],
+  },
+  {
+    img: ['/img/porfolio.png'],
+    title: 'Mi Portafolio',
+    titleEnglish: 'My Portfolio',
+    href: 'https://wilinston-quispe.vercel.app/',
+    dataList: ['TypeScript', 'Css', 'Next.js', 'React.js', 'Antdesign', 'Git'],
+  },
+  {
+    img: ['/img/api-ecommerce.png'],
+    title: 'Creacion de API',
+    titleEnglish: 'API creation',
+    description: 'creación de una API para una tienda online',
+    descriptionEnglish: 'creating an API for an online store',
+    dataList: ['Node.js', 'MongoDB', 'Graphql', 'Cloudinary', 'Mongoose'],
+  },
+  {
+    img: ['/img/YT-clone.png'],
+    title: 'YT Clone',
+    titleEnglish: 'YT Clone',
+    href: 'https://yt-clone-two.vercel.app/',
+    dataList: [
+      'TypeScript',
+      'Next.js',
+      'Css',
+      'React.js',
+      'Git',
+      'API Youtube',
+    ],
+  },
+  {
+    img: ['/img/Menus.png'],
+    title: "Menus Diarios Pa'ti",
+    titleEnglish: "Menus Diarios Pa'ti",
+    href: 'https://menus-diarios-pati.vercel.app/',
+    dataList: [
+      'TypeScript',
+      'Next.js',
+      'Css',
+      'React.js',
+      'Git',
+      'TailwindCSS',
+      'Apollo client',
+    ],
+  },
+  {
+    img: [
+      '/img/screen-1.png',
+      '/img/screen-2.png',
+      '/img/screen-3.png',
+      '/img/screen-4.png',
+    ],
+    title: 'iMakeUp',
+    titleEnglish: 'iMakeUp',
+    description:
+      'Una app que le permite a las usuarias probarse los productos de maquillaje y recibir asesoría con ayuda de realidad aumentada e inteligencia artificial.',
+    descriptionEnglish:
+      'An app that allows users to try on makeup products and receive advice with the help of augmented reality and artificial intelligence.',
+    dataList: [
+      'TypeScript',
+      'React Native',
+      'React Native Navigation',
+      'Git',
+      'App Gallery',
+    ],
+  },
+  {
+    img: [
+      '/img/LIMPIZIMO-1.png',
+      '/img/LIMPIZIMO-2.png',
+      '/img/LIMPIZIMO-3.png',
+      '/img/LIMPIZIMO-4.png',
+    ],
+    title: 'LIMPIZIMO',
+    titleEnglish: 'LIMPIZIMO',
+    description:
+      'Los usuarios pueden buscar personas que limpien, ordenas su casa lo puede hacer por horas y pueden ver su ubicación usando el mapa dentro del la app.',
+    descriptionEnglish:
+      'Users can search for people who clean, tidy their house can do it for hours and they can see their location using the map inside the app.',
+    dataList: ['React Native', 'React Native Navigation', 'Git'],
+  },
+]
+
 const Proyects = (): JSX.Element => {
   const { localLanguage } = useContext(LanguageContext)
   return (
@@ -18,114 +120,17 @@ const Proyects = (): JSX.Element => {
         )}
 
         <Row justify="center" gutter={[16, 16]}>
-          <ProyectsCard
-            img={['/img/Inicio-Goshop.png']}
-            title="Ecommerce venta de Ropas"
-            titleEnglish="Ecommerce sale of clothes"
-            href="https://mi-tienda-online.vercel.app/"
-            dataList={[
-              'JavaScript',
-              'React.js',
-              'Redux',
-              'Antdesign',
-              'Css',
-              'Bulma',
-              'Next.js',
-              'Git',
-              'Apollo Client',
-              'Paypal',
-              'Cloudinary',
-            ]}
-          />
-          <ProyectsCard
-            img={['/img/porfolio.png']}
-            title="Mi Portafolio"
-            titleEnglish="My Portfolio"
-            href="https://wilinston-quispe.vercel.app/"
-            dataList={[
-              'TypeScript',
-              'Css',
-              'Next.js',
-              'React.js',
-              'Antdesign',
-              'Git',
-            ]}
-          />
-          <ProyectsCard
-            img={['/img/api-ecommerce.png']}
-            title="Creacion de API"
-            titleEnglish="API creation"
-            description="creación de una API para una tienda online"
-            descriptionEnglish="creating an API for an online store"
-            dataList={[
-              'Node.js',
-              'MongoDB',
-              'Graphql',
-              'Cloudinary',
-              'Mongoose',
-            ]}
-          />
-          <ProyectsCard
-            img={['/img/YT-clone.png']}
-            title="YT Clone"
-            titleEnglish="YT Clone"
-            href="https://yt-clone-two.vercel.app/"
-            dataList={[
-              'TypeScript',
-              'Next.js',
-              'Css',
-              'React.js',
-              'Git',
-              'API Youtube',
-            ]}
-          />
-          <ProyectsCard
-            img={['/img/Menus.png']}
-            title="Menus Diarios Pa'ti"
-            titleEnglish="Menus Diarios Pa'ti"
-            href="https://menus-diarios-pati.vercel.app/"
-            dataList={[
-              'TypeScript',
-              'Next.js',
-              'Css',
-              'React.js',
-              'Git',
-              'TailwindCSS',
-              'Apollo client',
-            ]}
-          />
-          <ProyectsCard
-            img={[
-              '/img/screen-1.png',
-              '/img/screen-2.png',
-              '/img/screen-3.png',
-              '/img/screen-4.png',
-            ]}
-            title="iMakeUp"
-            titleEnglish="iMakeUp"
-            description={`Una app que le permite a las usuarias probarse los productos de maquillaje y recibir asesoría con ayuda de realidad aumentada e inteligencia artificial.`}
-            descriptionEnglish="An app that allows users to try on makeup products and receive advice with the help of augmented reality and artificial intelligence."
-            dataList={[
-              'TypeScript',
-              'React Native',
-              'React Native Navigation',
-              'Git',
-              'App Gallery',
-            ]}
-          />
-          <ProyectsCard
-            img={[
-              '/img/LIMPIZIMO-1.png',
-              '/img/LIMPIZIMO-2.png',
-              '/img/LIMPIZIMO-3.png',
-              '/img/LIMPIZIMO-4.png',
-            ]}
-            title="LIMPIZIMO"
-            titleEnglish="LIMPIZIMO"
-            description={`Los usuarios pueden buscar personas que limpien, ordenas su casa lo puede hacer por horas y pueden ver su ubicación usando el mapa dentro del la app.`}
-            descriptionEnglish="Users can search for people who clean, tidy their house can do it for hours and they can see their location using the map inside the app."
-            dataList={['React Native', 'React Native Navigation', 'Git']}
-          />
+          {DATA.reverse().map((d) => (
+            <ProyectsCard
+              img={d.img}
+              title={d.title}
+              titleEnglish={d.titleEnglish}
+              href={d.href}
+              description={d.description}
+              descriptionEnglish={d.descriptionEnglish}
+              dataList={d.dataList}
+            />
+          ))}
         </Row>
       </Col>
     </Row>
